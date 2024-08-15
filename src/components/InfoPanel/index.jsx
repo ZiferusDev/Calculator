1382fea5800dbc0235cde379207bf46c14a581c7
Igor <ir-red5@mail.ru>
2024-08-15 19:56:07 +0300

    feat: add close btn

--- src/components/InfoPanel/index.jsx
+++ src/components/InfoPanel/index.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import styles from "./infoPanel.module.css"
+import { useRef } from "react";
+import styles from "./infoPanel.module.css";
 
 const InfoPanel = () => {
+  const panelRef = useRef(null);
+  const closePanel = () => {
+    if (panelRef) {
+      console.log(panelRef);
+      panelRef.current.hidden = true;
+    }
+  };
   return (
-    <div className={styles.container}>
-        Hi! This is my fist project using React.
-        <br />
-        It may not work very well, 
-        <br />
-        but it performs the main functions as well as it can. 😎
+    <div className={styles.container} ref={panelRef}>
+      <button className={styles.closePanelBtn} onClick={closePanel}>
+        x
+      </button>
+      Hi! This is my fist project using React.
+      <br />
+      It may not work very well,
+      <br />
+      but it performs the main functions as well as it can. 😎
     </div>
-  )
-}
+  );
+};
 
-export default InfoPanel
+export default InfoPanel;
